7a0906b1b072323572880e70f0c43e12d1fac71e
shanikalakshani <42064743+shanikalakshani@users.noreply.github.com>
2019-02-16 10:17:59 +0530

    Add files via upload

---
+++ data-analysis.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import numpy as np # numerical computing 
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt #visualization
+import seaborn as sns #modern visualization
+
+#Sample
+
+sns.set_style("darkgrid")
+plt.rcParams['figure.figsize'] = (14, 8)
+
+file_path = 'C:\\Users\\INOVA\\Desktop\\Assignment\\'
+matches = pd.read_csv(file_path+'PSID.csv')
+
+print(matches)
+
+matches.shape
+print(matches.shape)
+
+matches.info()
+
+matches.describe()
+print(matches.describe())
+
+matches.head(2)
+#print(matches.head(2))
+
+matches['Seq No'].max()
+
+matches['educatn'].unique()
+
+print(matches['educatn'].unique())
+
+sns.countplot(x='educatn', data=matches)
+plt.show()
+
+np.random.choice(matches['Seq No'], 100)
+print(np.random.choice(matches['Seq No'], 100))
+
